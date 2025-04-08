eadd8b342b076ba2d956f9f20e6a465cf2a544b5
MarriamYusuf <marriamyusuf786@gmail.com>
2025-04-08 18:38:04 +0400

    Update broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") # Changed: Added a closing quote " for the string.
+if greeting in ["Arrr!"]: # Changed: Replaced the closing parenthesis 0 with a closing bracket ].
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+elif:
+	print("Greetings, hater of pirates!") # Changed: Indent
